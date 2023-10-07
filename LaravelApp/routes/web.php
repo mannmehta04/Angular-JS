d10214c4a5840036cd5be36c00c8a6437da4b708
Mannmehta04 <mannmehta04@gmail.com>
2023-10-07 15:00:04 +0530

    <Summary Undefined>
    <Summary undefined, uploaded in case of Device Transfer(07/10/2023), This project was performed as part of Curriculum (kind of.)>

---
+++ LaravelApp/routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+// Can add middle ware to forcefully redirect to new page
+// One way to add group or another to use controller
+Route::get('/contact', function () {
+    return "ok you're in";
+});
+
+Route::get('test', function () {
+    return ('Sup');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
